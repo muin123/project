c2d888abc1e21a3bc0433316ea031d6c38843617
muin <36788136+muin123@users.noreply.github.com>
2019-11-02 21:56:42 +0700

    Update sape.js

--- sape.js
+++ sape.js
@@ -121,7 +121,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://tipsku05.bblogspot.com/p/safelink.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://viomagg.blogspot.com/p/sapelink.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
